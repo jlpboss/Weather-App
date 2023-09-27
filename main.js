600d554b0f29f5807b9e46a2ba04286e22d0df1e
Jett Pennington <jettpennington@gmail.com>
2023-09-27 18:43:42 +0000

    added renderer and base html

---
+++ main.js
@@ -0,0 +1,44 @@
+let renderer = {
+    clearPage: function (div) {
+        let page = document.getElementById(div)
+        let childNodes = page.childNodes;
+        for (let i = childNodes.length - 1; i >= 0; i--) {
+            page.removeChild(childNodes[i]);
+        }
+    },
+    makeTag: function (elem, id, where, classAttribute) {
+        let cont = document.createElement(elem);
+        let out = document.getElementById(where);
+        cont.setAttribute("id", id);
+        cont.setAttribute("class", classAttribute);
+        out.appendChild(cont);
+    },
+    drawText: function (text, where, classAttribute = "") {
+        let para = document.createElement("p");
+        let node = document.createTextNode(text);
+        let out = document.getElementById(where);
+        para.setAttribute("class", classAttribute);
+        para.appendChild(node);
+        out.appendChild(para);
+    },
+    makeEvent: function (listening, event, funct, param = "()") {
+        let element = document.getElementById(listening);
+        element.addEventListener(event, function () {
+            eval(funct + param); 
+        });
+    },
+    makeContainer: function (id, where, containerClass = "container", rowClass = "row", colClass = "col") {
+        this.makeTag("div", id + "Cont", where, containerClass)
+        this.makeTag("div", id + "Row", id + "Cont", rowClass)
+        this.makeTag("div", id + "Col", id + "Row", colClass)
+    },
+    setAtrubute: function (tagId, atrubute, value){
+        let tag = document.getElementById(tagId);
+        tag.setAttribute(atrubute, value);
+    },
+    drawImage: function (imgLink, id, where, classAttribute, altText){
+        renderer.makeTag("img", id, where, classAttribute)
+        renderer.setAtrubute(id, "src", imgLink)
+        renderer.setAtrubute(id, "alt", altText)
+    }
+};
