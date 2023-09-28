8bfeed2c38230fbd3fd2d08244bb0400524e6c87
Jett Pennington <jettpennington@gmail.com>
2023-09-28 17:02:14 +0000

    workinf*

--- main.js
+++ main.js
@@ -13,18 +13,16 @@ let renderer = {
         cont.setAttribute("class", classAttribute);
         out.appendChild(cont);
     },
-    drawText: function (text, where, classAttribute = "") {
-        let para = document.createElement("p");
+    drawText: function (text, where) {
         let node = document.createTextNode(text);
         let out = document.getElementById(where);
-        para.setAttribute("class", classAttribute);
-        para.appendChild(node);
-        out.appendChild(para);
+        out.appendChild(node);
+  
     },
     makeEvent: function (listening, event, funct, param = "()") {
         let element = document.getElementById(listening);
         element.addEventListener(event, function () {
-            eval(funct + param); 
+            eval(funct + param);
         });
     },
     makeContainer: function (id, where, containerClass = "container", rowClass = "row", colClass = "col") {
@@ -32,30 +30,97 @@ let renderer = {
         this.makeTag("div", id + "Row", id + "Cont", rowClass)
         this.makeTag("div", id + "Col", id + "Row", colClass)
     },
-    setAtrubute: function (tagId, atrubute, value){
+    setAtrubute: function (tagId, atrubute, value) {
         let tag = document.getElementById(tagId);
         tag.setAttribute(atrubute, value);
     },
-    drawImage: function (imgLink, id, where, classAttribute, altText){
+    drawImage: function (imgLink, id, where, classAttribute, altText) {
         renderer.makeTag("img", id, where, classAttribute)
         renderer.setAtrubute(id, "src", imgLink)
         renderer.setAtrubute(id, "alt", altText)
+    },
+    drawContentBox: function (where, id, headText, numberofSubBoxes, SubBoxTextArray){
+        this.makeContainer(id, where, "container", "row ", "col")
+        this.drawText(headText, id + "Col")
+        this.makeTag("div", id + "Row2", id + "Cont", "row")
+        for(let i = 0; i < numberofSubBoxes; i++){
+            this.makeTag("div", id + "Col" + i, id + "Row2", "col-" + (12/numberofSubBoxes))
+            this.drawText(SubBoxTextArray[i], id + "Col" + i)
+        }
     }
 };
 
 let weatherAPI = {
-    
-    apitest: function(){
-        const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?zip={40508},{1}&appid={34673af0c28d949d581b71e45eea6444}';
 
+    apiKey: "34673af0c28d949d581b71e45eea6444",
+
+    weather: {},
+
+    userLon: 0,
+
+    userLat: 0,
+
+    getWeatherDataFromGPS: async function () {
+        await this.getLocation()
+        
+        setTimeout(() => {
+            console.log(weatherAPI.userLat)
+            console.log(weatherAPI.userLon)
+            const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + this.userLat + '&lon=' + this.userLon + '&appid=' + this.apiKey;
+
+            axios.get(apiUrl)
+                .then(response => {
+                    this.weather = response.data;
+                    console.log(this.weather);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }, "1000");
+    },
+    getLocation: async function () {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                weatherAPI.userLat = position.coords.latitude;
+                weatherAPI.userLon = position.coords.longitude;
+
+                console.log(position.coords.latitude);
+                console.log(position.coords.longitude);
+
+                console.log(weatherAPI.userLat)
+                console.log(weatherAPI.userLon)
+            }, function (error) {
+                console.error("Error getting location: " + error.message);
+            });
+        } else {
+            console.error("Geolocation is not available in this browser.");
+        }
+    },
+    getWeatherDataFromZipCode: async function (zipCode) {
+
+        const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&appid=' + this.apiKey;
         axios.get(apiUrl)
-        .then(response => {
-            console.log('Data:', response.data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            .then(response => {
+                this.weather = response.data;
+                console.log(this.weather);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
 }
 
-weatherAPI.apitest();
+let weatherCard = {
+    makeCard: function(where){
+        renderer.makeTag("div", "weatherCard", where, "card text-center h5 cardStuff")
+        renderer.drawContentBox("weatherCard", "city", "City:", 1, [weatherAPI.weather.name])
+        renderer.drawContentBox("weatherCard", "temp", "Temperature:", 2, [(weatherAPI.weather.main.temp - 273.15).toFixed(2) + " Degrees Celsius",((weatherAPI.weather.main.temp - 273.15) * 9/5 + 32).toFixed(2) + " Degrees Fahrenheit"])
+        renderer.drawContentBox("weatherCard", "cond", "Condition:", 1, [weatherAPI.weather.weather[0].description])
+    }
+}
+
+weatherAPI.getWeatherDataFromGPS();
+
+setTimeout(() => {
+    weatherCard.makeCard("div1");
+}, "2000");
