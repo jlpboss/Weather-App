2393c50b70adeb145d99e662366225638597abca
Jett Pennington <jettpennington@gmail.com>
2023-09-29 13:48:42 +0000

    almost done

--- main.js
+++ main.js
@@ -50,73 +50,92 @@ let renderer = {
     }
 };
 
-class WeatherAPI {
-    constructor() {
-        this.apiKey = "34673af0c28d949d581b71e45eea6444";
-        this.weather = {};
-
+let weatherAPI = {
+    apiKey: "34673af0c28d949d581b71e45eea6444",
+  
+    weather: {},
+  
+    userLon: 0,
+  
+    userLat: 0,
+  
+    getWeatherDataFromGPS: async function (whereToMakeCard) {
+      let [lat, lon] = await this.getLocation();
+      console.log({ lat, lon });
+      const apiUrl =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        this.apiKey;
+  
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          this.weather = response.data;
+          console.log(this.weather);
+          weatherCard.makeCard(whereToMakeCard);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+    getLocation: async function () {
+      // let lat, lon;
+      if ("geolocation" in navigator) {
+        return new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(
+            function (position) {
+              console.log(position.coords.latitude);
+              console.log(position.coords.longitude);
+              //   lat = position.coords.latitude;
+              //   lon = position.coords.longitude;
+              resolve([position.coords.latitude, position.coords.longitude]);
+            },
+            function (error) {
+              console.error("Error getting location: " + error.message);
+              reject("Error getting location: " + error.message);
+            }
+          );
+        });
+  
+        //   return [lat, lon];
+      } else {
+        console.error("Geolocation is not available in this browser.");
+      }
     }
-    async getWeatherDataFromGPS(x) {
-        // let x = geolocationClassObj.getLocation();
-        console.log("a")
-        console.log(x)
-        // const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + x[0] + '&lon=' + x[1] + '&appid=34673af0c28d949d581b71e45eea6444';
-        await axios.get(x)
-            .then(response => {
-                this.weather = response.data;
-                console.log(this.weather);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
-}
-class geolocationClass {
-    constructor() {
-        this.latitude = 0;
-        this.longitude = 0;
-        this.getLocation();
-        this.y = "";
-    }
-    async getLocation() {
-        let y;
-        let nav = navigator.geolocation;
-        nav.getCurrentPosition(this.makeURL(position));
-        console.log('url:', this.y);
-        return this.y;
-    }
-
-    async makeURL(position) {
-        // weatherAPI.userLat = position.coords.latitude;
-        // weatherAPI.userLon = position.coords.longitude;
-
-        console.log('lat:', position.coords.latitude);
-        console.log('lon:', position.coords.longitude);
-        // this.latitude = position.coords.latitude;
-        // this.longitude = position.coords.longitude;
-
-        // return [position.coords.latitude, position.coords.longitude];
-        this.y = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=34673af0c28d949d581b71e45eea6444`;
-        console.log(y)
-        // console.log(weatherAPI.userLat)
-        // console.log(weatherAPI.userLon)
-    }
-}
+  };
 let weatherCard = {
     makeCard: function (where) {
-        renderer.makeTag("div", "weatherCard", where, "card text-center h5 cardStuff")
+        renderer.makeTag("div", "weatherCard", where, "card text-center h5 cardStuff border border-success rounded")
         renderer.drawContentBox("weatherCard", "city", "City:", 1, [weatherAPI.weather.name])
         renderer.drawContentBox("weatherCard", "temp", "Temperature:", 2, [(weatherAPI.weather.main.temp - 273.15).toFixed(2) + " Degrees Celsius", ((weatherAPI.weather.main.temp - 273.15) * 9 / 5 + 32).toFixed(2) + " Degrees Fahrenheit"])
         renderer.drawContentBox("weatherCard", "cond", "Condition:", 1, [weatherAPI.weather.weather[0].description])
+    },
+    makeTestCard: function(id, where){
+        renderer.makeTag("div", id, where, "card text-center h5 cardStuff border border-danger rounded")
+
+        renderer.makeTag("div", id + "Body", id, "card-body")
+        renderer.makeTag("h5", id + "Title", id + "Body", "card-title")
+        renderer.drawText("Card title", id + "Title")
+
+        renderer.makeTag("h6", id + "Subtitle", id + "Body", "card-subtitle mb-2")
+        renderer.drawText("Card subtitle", id + "Subtitle")
+
+        renderer.makeTag("p", id + "Text", id + "Body", "card-subtitle mb-2")
+        renderer.drawText("Some quick example text to build on the card title and make up the bulk of the card's content.", id + "Text")
     }
 }
-let geolocationClassObj = new geolocationClass();
-let weatherAPI = new WeatherAPI();
-weatherAPI.getWeatherDataFromGPS(geolocationClassObj.getLocation());
-// weatherAPI.getLocation().then()
-//weatherAPI.getWeatherDataFromGPS(res)
-//async function test() {}
-//weatherAPI.getWeatherDataFromGPS();
-// setTimeout(() => {
-//     weatherCard.makeCard("div1");
-// }, "2000");
+
+renderer.makeContainer("main", "div1", "container", "row  d-flex justify-content-center", "col-12 md-col-6")
+
+renderer.makeTag("div", "mainCol2", "mainRow", "col-12 md-col-6")
+renderer.makeTag("div", "mainCol3", "mainRow", "col-12 md-col-6")
+renderer.makeTag("div", "mainCol4", "mainRow", "col-12 md-col-6")
+
+weatherCard.makeTestCard("testCard1", "mainCol")
+weatherCard.makeTestCard("testCard2", "mainCol2")
+weatherCard.makeTestCard("testCard3", "mainCol3")
+weatherAPI.getWeatherDataFromGPS("mainCol4");
+
